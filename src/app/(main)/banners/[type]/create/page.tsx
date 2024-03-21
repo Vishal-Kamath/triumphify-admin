4bdf8d3fa9e56e6e6a8dc75a3af342b593fef2ec
Vishal-Kamath <vishalkamath853@gmail.com>
2024-03-21 21:00:50 +0530

    ⬆️update: tab component UI

--- src/app/(main)/banners/[type]/create/page.tsx
+++ src/app/(main)/banners/[type]/create/page.tsx
@@ -5,32 +5,31 @@ import Link from "next/link";
 import { FC } from "react";
 import CreateMainBannerForm from "./create-banner-form";
 import PrivilageProvider from "@/components/providers/privilage.provider";
+import TabComponent from "@/components/misc/component";
 
 const CreateMainBannerPage: FC<{
   params: { type: "main" | "sub" };
 }> = ({ params }) => {
   return (
     <PrivilageProvider path="/banners/:type/create">
-      <div className="h-full min-h-screen w-full p-6 pb-12">
-        <main className="flex h-full w-full flex-col gap-6 rounded-lg border-1 border-slate-200 bg-white p-6 shadow-md">
-          <div className="flex items-center gap-3">
-            <Link
-              href={`/banners/${params.type}`}
-              className={buttonVariants({ variant: "ghost" })}
-            >
-              <MoveLeft />
-            </Link>
-            <div className="flex flex-col">
-              <h3 className="text-xl font-medium">Create New Banner</h3>
-              <p className="text-sm text-gray-500">
-                Input banner details to create a new banner
-              </p>
-            </div>
+      <TabComponent>
+        <div className="flex items-center gap-3">
+          <Link
+            href={`/banners/${params.type}`}
+            className={buttonVariants({ variant: "ghost" })}
+          >
+            <MoveLeft />
+          </Link>
+          <div className="flex flex-col">
+            <h3 className="text-xl font-medium">Create New Banner</h3>
+            <p className="text-sm text-gray-500">
+              Input banner details to create a new banner
+            </p>
           </div>
-          <Separator />
-          <CreateMainBannerForm type={params.type} />
-        </main>
-      </div>
+        </div>
+        <Separator />
+        <CreateMainBannerForm type={params.type} />
+      </TabComponent>
     </PrivilageProvider>
   );
 };
--- src/components/misc/component.tsx
+++ src/components/misc/component.tsx
@@ -4,11 +4,11 @@ import { FC, HTMLAttributes } from "react";
 interface TabProps extends HTMLAttributes<HTMLDivElement> {}
 const TabComponent: FC<TabProps> = ({ className, children, ...props }) => {
   return (
-    <div className="h-full w-full lg:p-6 lg:pb-12">
+    <div className="h-full w-full bg-slate-50 lg:p-6 lg:pb-12">
       <main
         className={cn(
           "flex h-full w-full flex-col gap-6 bg-white p-6 shadow-md max-lg:pb-12 lg:rounded-lg lg:border-1 lg:border-slate-200",
-          className,
+          className
         )}
         {...props}
       >
