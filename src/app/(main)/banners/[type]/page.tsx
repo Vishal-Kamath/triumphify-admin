1395bba2d687b8a80ac4f8c2412363fc31067c3a
Vishal-Kamath <vishalkamath853@gmail.com>
2024-03-21 20:58:18 +0530

    ⬆️update: banner tables ui

--- src/app/(main)/banners/[type]/page.tsx
+++ src/app/(main)/banners/[type]/page.tsx
@@ -1,19 +1,19 @@
 import { FC } from "react";
 import BannerTable from "./banner-table";
 import PrivilageProvider from "@/components/providers/privilage.provider";
+import { Separator } from "@/components/ui/separator";
 
 const MainBannerPage: FC<{
   params: { type: "main" | "sub" };
 }> = ({ params }) => {
   return (
     <PrivilageProvider path="/banners/:type">
-      <main className="flex h-full flex-col bg-white">
-        <div className="flex h-12 w-full items-center justify-between border-b-1 border-slate-300 px-6">
-          <h2 className="text-lg font-semibold">
-            {params.type[0].toUpperCase() + params.type.slice(1)} Banner Details
-          </h2>
-        </div>
-        <div className="p-6">
+      <main className="flex h-full min-h-full w-full flex-col gap-6 bg-white p-6">
+        <h2 className="text-lg font-semibold leading-none">
+          {params.type[0].toUpperCase() + params.type.slice(1)} Banner Details
+        </h2>
+        <Separator />
+        <div className="h-full">
           <BannerTable type={params.type} />
         </div>
       </main>
