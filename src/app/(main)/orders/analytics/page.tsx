fd4a530fde381f9a12f10171fbc7e9b30c22901a
Vishal-Kamath <vishalkamath853@gmail.com>
2024-03-26 14:03:51 +0530

    ❌remove: analytics table

--- src/app/(main)/orders/analytics/page.tsx
+++ src/app/(main)/orders/analytics/page.tsx
@@ -1,15 +1,11 @@
 import { Separator } from "@/components/ui/separator";
 import { FC } from "react";
-import AnalyticsPageTable from "./analytics-table";
 
 const AnalyticsPage: FC = () => {
   return (
     <main className="flex h-full min-h-full w-full flex-col gap-6 bg-white p-6">
       <h2 className="text-lg font-semibold leading-none">Order Analytics</h2>
       <Separator />
-      <div className="h-full">
-        <AnalyticsPageTable />
-      </div>
     </main>
   );
 };
