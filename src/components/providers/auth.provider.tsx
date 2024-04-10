c36b9dc029551a2212ca933fd3403c2816b165d5
Vishal-Kamath <vishalkamath853@gmail.com>
2024-04-10 15:59:38 +0530

    ⬆️update: auth provider login

--- src/components/providers/auth.provider.tsx
+++ src/components/providers/auth.provider.tsx
@@ -5,6 +5,8 @@ import { isServer } from "@tanstack/react-query";
 import { FC, ReactNode } from "react";
 import { Dialog, DialogClose, DialogContent } from "@/components/ui/dialog";
 import LoginForm from "@/app/(auth)/auth/login/login-form";
+import Image from "next/image";
+import Logo from "../misc/logo";
 
 const AuthProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const { data, isLoading, isFetched } = useMe();
@@ -21,10 +23,20 @@ const AuthProvider: FC<{ children: ReactNode }> = ({ children }) => {
     return (
       <Dialog open={true}>
         <DialogContent
+          className="overflow-hidden sm:rounded-[1.75rem]"
           closeable={false}
-          className="flex items-center justify-center py-10"
         >
-          <LoginForm inline={true} />
+          <div className="relative isolate flex flex-col items-center justify-center gap-6 pb-4">
+            <Image
+              alt="auth background image"
+              src="/auth-bg.svg"
+              width={500}
+              height={500}
+              className="fixed left-0 top-0 -z-10 h-full w-full object-cover"
+            />
+            <Logo className="max-w-[12.5rem]" />
+            <LoginForm inline={true} />
+          </div>
         </DialogContent>
       </Dialog>
     );
