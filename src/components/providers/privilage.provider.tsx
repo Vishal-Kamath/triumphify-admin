67d7341a1e63a55b48362aeb3794495479db61e6
Vishal-Kamath <vishalkamath853@gmail.com>
2024-04-10 20:02:51 +0530

    ⬆️update: privilage provider update

--- src/components/providers/privilage.provider.tsx
+++ src/components/providers/privilage.provider.tsx
@@ -4,6 +4,9 @@ import { isServer } from "@tanstack/react-query";
 import { FC, ReactNode } from "react";
 import { Dialog, DialogContent } from "@/components/ui/dialog";
 import { usePrivilage } from "@/lib/privilage";
+import Logo from "../misc/logo";
+import Link from "next/link";
+import { ShieldAlert } from "lucide-react";
 
 const PrivilageProvider: FC<{ path: string; children: ReactNode }> = ({
   path,
@@ -16,11 +19,25 @@ const PrivilageProvider: FC<{ path: string; children: ReactNode }> = ({
       <Dialog open={true}>
         <DialogContent
           closeable={false}
-          className="flex items-center justify-center py-10"
+          className="flex items-center justify-start gap-6 flex-col py-9"
         >
-          <h1 className="text-center text-xl font-medium">
-            {privilage.description}
-          </h1>
+          {/* <Logo className="w-[12.5rem]" /> */}
+
+          <div className="flex w-full gap-3 justify-center items-center text-red-700">
+            <ShieldAlert className="size-12" />
+            <h1 className="text-4xl font-semibold">Unauthorized!!</h1>
+          </div>
+          <p className="text-center text-slate-700">
+            You do not have the necessary permissions to access this resource
+            with your current role. Please contact your administrator for
+            assistance.
+          </p>
+          <Link
+            href="/"
+            className="text-blue-600 text-xs underline underline-offset-2 hover:text-blue-900"
+          >
+            Back to Home
+          </Link>
         </DialogContent>
       </Dialog>
     );
