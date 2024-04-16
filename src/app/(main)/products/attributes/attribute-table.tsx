ec694e35dd68ba005780a3cbdd1bbd35e429d667
Vishal-Kamath <vishalkamath853@gmail.com>
2024-04-16 12:35:09 +0530

    ⬆️update: attributes

--- src/app/(main)/products/attributes/attribute-table.tsx
+++ src/app/(main)/products/attributes/attribute-table.tsx
@@ -1,34 +1,28 @@
 "use client";
 
-import { Attribute } from "@/@types/attribute";
-import DataTable from "@/components/data-table/data-table";
-import { DataTablePagination } from "@/components/data-table/data-table-pagination";
-import DataTableToolbar from "@/components/data-table/data-table-toolbar";
-import ConfirmDelete from "@/components/misc/confirmDelete";
-import { DataTableSkeleton } from "@/components/data-table/data-table-skeleton";
-import { buttonVariants } from "@/components/ui/button";
-import { useToast } from "@/components/ui/use-toast";
-import { invalidateAllAttributes, useAttributes } from "@/lib/attributes";
-import { cn } from "@/lib/utils";
-import { dateFormater } from "@/utils/dateFormater";
 import {
-  ColumnDef,
-  ColumnFiltersState,
-  SortingState,
-  VisibilityState,
-  getCoreRowModel,
-  getFacetedRowModel,
-  getFacetedUniqueValues,
-  getFilteredRowModel,
-  getPaginationRowModel,
-  getSortedRowModel,
-  useReactTable,
-} from "@tanstack/react-table";
-import axios from "axios";
-import { PencilLine, Plus, Trash2 } from "lucide-react";
+  MRT_ColumnDef,
+  MRT_Row,
+  MaterialReactTable,
+  useMaterialReactTable,
+} from "material-react-table";
+import { FC, useMemo } from "react";
+import { dateFormater } from "@/utils/dateFormater";
+import { Button, IconButton, Tooltip } from "@mui/material";
+import { Add, FileDownload, Refresh } from "@mui/icons-material";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { useMe } from "@/lib/auth";
+import { handleExtract } from "@/utils/extract";
 import Link from "next/link";
-import { FC, useState } from "react";
-import { RxCaretSort } from "react-icons/rx";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
+import { PencilLine, Trash2 } from "lucide-react";
+import ConfirmDelete from "@/components/misc/confirmDelete";
+import { useToast } from "@/components/ui/use-toast";
+import axios from "axios";
+import { invalidateAllAttributes, useAttributes } from "@/lib/attributes";
+import { Attribute } from "@/@types/attribute";
 
 const AttributesTable: FC = () => {
   const { toast } = useToast();
@@ -55,124 +49,220 @@ const AttributesTable: FC = () => {
       });
   };
 
-  const columns: ColumnDef<Attribute>[] = [
-    {
-      header: ({ column }) => {
-        return (
-          <button
-            className="flex items-center gap-2 border-none bg-transparent p-0 outline-none focus:outline-none"
-            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-          >
-            Name
-            <RxCaretSort className="ml-2 h-4 w-4" />
-          </button>
-        );
+  const {
+    data: attributes,
+    isLoading,
+    refetch,
+    isRefetching,
+  } = useAttributes();
+  const { data: me } = useMe();
+
+  const handleExportRows = (rows: MRT_Row<Attribute>[]) => {
+    const rowData = rows.map((row) => row.original);
+    handleExtract("attributes-data", rowData);
+  };
+
+  const handleExportData = () => {
+    if (!attributes) return;
+    handleExtract("attributes-data", attributes);
+  };
+
+  const columns = useMemo<MRT_ColumnDef<Attribute>[]>(
+    () => [
+      {
+        header: "Name",
+        accessorKey: "name",
+        enableHiding: false,
       },
-      accessorKey: "name",
-      enableHiding: false,
-      enableSorting: true,
-    },
-    {
-      header: "Values",
-      accessorKey: "values",
-      cell: ({ row }) => {
-        const values = row.getValue("values");
-        return (
-          <div className="max-w-md">
-            {Array.isArray(values) && !!values.length
-              ? values.map((value) => value.name).join(", ")
-              : "N/A"}
+      {
+        header: "Values",
+        accessorKey: "values",
+        accessorFn: (originalRow) =>
+          originalRow.values.map((val) => val.name).join(", "),
+        Cell: ({ row }) => {
+          return (
+            <div className="text-wrap max-h-28 overflow-y-auto min-w-[300px] max-w-[300px] scrollbar-thin">
+              {row.getValue("values")}
+            </div>
+          );
+        },
+      },
+      {
+        header: "Created At",
+        accessorKey: "created_at",
+        filterVariant: "date-range",
+        accessorFn: (originalRow) => new Date(originalRow.created_at),
+        Cell: ({ row }) => dateFormater(new Date(row.getValue("created_at"))),
+      },
+      {
+        header: "Updated At",
+        accessorKey: "updated_at",
+        filterVariant: "date-range",
+        accessorFn: (originalRow) =>
+          originalRow.updated_at ? new Date(originalRow.updated_at) : "null",
+        Cell: ({ row }) =>
+          row.getValue("updated_at") !== "null"
+            ? dateFormater(new Date(row.getValue("updated_at")))
+            : "N/A",
+      },
+      {
+        accessorKey: "id",
+        header: "",
+        enableHiding: false,
+        enableColumnFilter: false,
+        enableSorting: false,
+        Cell: ({ row }) => (
+          <div className="flex">
+            <Link
+              href={`/products/attributes/${row.getValue("id")}`}
+              className={cn(buttonVariants({ variant: "ghost" }))}
+            >
+              <PencilLine className="h-4 w-4" />
+            </Link>
+            <ConfirmDelete
+              className="flex aspect-square h-10 w-10 items-center justify-center rounded-md hover:bg-red-100 hover:text-red-500"
+              confirmText={row.getValue("name")}
+              deleteFn={() => handleDeleteAttribute(row.getValue("id"))}
+            >
+              <Trash2 className="h-4 w-4" />
+            </ConfirmDelete>
           </div>
-        );
+        ),
       },
-    },
-    {
-      header: "Created At",
-      accessorKey: "created_at",
-      cell: ({ row }) => dateFormater(new Date(row.getValue("created_at"))),
-    },
-    {
-      header: "Updated At",
-      accessorKey: "updated_at",
-      cell: ({ row }) =>
-        row.getValue("updated_at")
-          ? dateFormater(new Date(row.getValue("updated_at")))
-          : "N/A",
-    },
-    {
-      accessorKey: "id",
-      header: "",
-      enableHiding: false,
-      enableSorting: false,
-      cell: ({ row }) => (
-        <div className="flex">
-          <Link
-            href={`/products/attributes/${row.getValue("id")}`}
-            className={cn(buttonVariants({ variant: "ghost" }))}
-          >
-            <PencilLine className="h-4 w-4" />
-          </Link>
-          <ConfirmDelete
-            className="flex aspect-square h-10 w-10 items-center justify-center rounded-md hover:bg-red-100 hover:text-red-500"
-            confirmText={row.getValue("name")}
-            deleteFn={() => handleDeleteAttribute(row.getValue("id"))}
-          >
-            <Trash2 className="h-4 w-4" />
-          </ConfirmDelete>
-        </div>
-      ),
-    },
-  ];
-
-  const [sorting, setSorting] = useState<SortingState>([]);
-  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
-  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
-
-  const { data: attributes, isLoading, refetch } = useAttributes();
-
-  const table = useReactTable({
-    data: attributes || [],
+    ],
+    [attributes]
+  );
+  const table = useMaterialReactTable({
     columns,
-    onSortingChange: setSorting,
-    onColumnFiltersChange: setColumnFilters,
-    getCoreRowModel: getCoreRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
-    getSortedRowModel: getSortedRowModel(),
-    getFilteredRowModel: getFilteredRowModel(),
-    onColumnVisibilityChange: setColumnVisibility,
-    getFacetedRowModel: getFacetedRowModel(),
-    getFacetedUniqueValues: getFacetedUniqueValues(),
-    state: {
-      sorting,
-      columnFilters,
-      columnVisibility,
-    },
-  });
+    data: attributes || [],
+    muiTablePaperProps: ({ table }) => ({
+      elevation: 0,
+      style: {
+        zIndex: table.getState().isFullScreen ? 1000 : undefined,
+      },
+    }),
+    renderTopToolbarCustomActions: ({ table }) => (
+      <div className="flex gap-2">
+        {me?.role === "superadmin" ? (
+          <>
+            <Link href="/products/attributes/create">
+              <Button
+                startIcon={<Add />}
+                sx={{
+                  "@media (max-width: 768px)": {
+                    display: "none",
+                  },
+                }}
+              >
+                Add Attribute
+              </Button>
+            </Link>
+            <Button
+              onClick={handleExportData}
+              className="max-md:hidden"
+              startIcon={<FileDownload />}
+              sx={{
+                "@media (max-width: 768px)": {
+                  display: "none",
+                },
+              }}
+            >
+              Export All Data
+            </Button>
+            <Button
+              disabled={
+                !table.getIsSomeRowsSelected() && !table.getIsAllRowsSelected()
+              }
+              onClick={() => handleExportRows(table.getSelectedRowModel().rows)}
+              startIcon={<FileDownload />}
+              sx={{
+                "@media (max-width: 768px)": {
+                  display: "none",
+                },
+              }}
+            >
+              Export Selected Rows
+            </Button>
 
-  return isLoading ? (
-    <DataTableSkeleton columnCount={columns.length} />
-  ) : (
-    <div className="flex w-full flex-col gap-4">
-      <DataTableToolbar
-        table={table}
-        searchUsing="name"
-        actionNodes={
-          <Link
-            href="/products/attributes/create"
-            className={cn(
-              buttonVariants({ variant: "secondary" }),
-              "gap-2 hover:bg-fuchsia-100 hover:text-fuchsia-700"
-            )}
+            <Link href="/products/attributes/create">
+              <Tooltip
+                sx={{
+                  "display": "none",
+                  "@media (max-width: 768px)": {
+                    display: "block",
+                  },
+                  "height": "40px",
+                }}
+                arrow
+                title="Add Attribute"
+              >
+                <IconButton>
+                  <Add className="-translate-y-2" />
+                </IconButton>
+              </Tooltip>
+            </Link>
+            <Tooltip
+              sx={{
+                "display": "none",
+                "@media (max-width: 768px)": {
+                  display: "block",
+                },
+                "height": "40px",
+              }}
+              arrow
+              title="Export All Data"
+            >
+              <IconButton onClick={() => handleExportData()}>
+                <FileDownload className="-translate-y-2" />
+              </IconButton>
+            </Tooltip>
+            <Tooltip
+              sx={{
+                "display": "none",
+                "@media (max-width: 768px)": {
+                  display: "block",
+                },
+                "height": "40px",
+              }}
+              arrow
+              title="Export Selected Rows"
+            >
+              <IconButton
+                disabled={
+                  !table.getIsSomeRowsSelected() &&
+                  !table.getIsAllRowsSelected()
+                }
+                onClick={() =>
+                  handleExportRows(table.getSelectedRowModel().rows)
+                }
+              >
+                <FileDownload className="-translate-y-2" />
+              </IconButton>
+            </Tooltip>
+          </>
+        ) : null}
+        <Tooltip arrow title="Refresh Data">
+          <IconButton
+            sx={{
+              height: "40px",
+            }}
+            onClick={() => refetch()}
           >
-            <Plus className="h-4 w-4" />
-            <span>Create</span>
-          </Link>
-        }
-        refetch={refetch}
-      />
-      <DataTable table={table} columnSpan={columns.length} />
-      <DataTablePagination table={table} />
-    </div>
+            <Refresh />
+          </IconButton>
+        </Tooltip>
+      </div>
+    ),
+    state: {
+      showProgressBars: isRefetching,
+      isLoading,
+    },
+    enableRowSelection: me?.role === "superadmin",
+  });
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <MaterialReactTable table={table} />
+    </LocalizationProvider>
   );
 };
 
