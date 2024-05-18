016617d7e14fb2d0f6dac00d2987204e6ac490fe
Vishal-Kamath <vishalkamath853@gmail.com>
2024-05-18 12:42:06 +0530

    ⬆️update: if logs not found

--- src/app/(main)/employees/logs/logs.tsx
+++ src/app/(main)/employees/logs/logs.tsx
@@ -45,36 +45,42 @@ const Logs: FC = () => {
         <Skeleton className="h-24 w-full rounded-md" />
       </>
     );
-  return (logs || []).map((log, index) => (
-    <div
-      key={log.name + index}
-      className="flex flex-col gap-6 rounded-md border-1 border-slate-200 p-4 shadow-sm transition-all duration-200 ease-in-out hover:shadow-md"
-    >
-      <div className="flex gap-3">
-        <span>{log.name}</span>
-        <Link
-          className="ml-auto rounded-md bg-slate-50 p-1 text-slate-500 hover:bg-sky-100 hover:text-sky-600"
-          href={`/employees/logs/${log.name}`}
-        >
-          <MoreHorizontal className="h-4 w-4" />
-        </Link>
-        <ConfirmDelete
-          confirmText={log.name}
-          deleteFn={() => handleDeleteLogs(log.name)}
-          className="rounded-md bg-slate-50 p-1 text-slate-500 hover:bg-red-100 hover:text-red-600"
-        >
-          <Trash2 className="h-4 w-4" />
-        </ConfirmDelete>
-      </div>
-      <div className="flex justify-between gap-3 text-xs text-slate-500">
-        <span>Size: {log.size}</span>
-        <div className="flex items-center gap-2">
-          <CalendarDays className="h-3 w-3 text-slate-400" />
-          <span>{dateFormater(new Date(log.createdAt))}</span>
+  return logs?.length ? (
+    (logs || []).map((log, index) => (
+      <div
+        key={log.name + index}
+        className="flex flex-col gap-6 rounded-md border-1 border-slate-200 p-4 shadow-sm transition-all duration-200 ease-in-out hover:shadow-md"
+      >
+        <div className="flex gap-3">
+          <span>{log.name}</span>
+          <Link
+            className="ml-auto rounded-md bg-slate-50 p-1 text-slate-500 hover:bg-sky-100 hover:text-sky-600"
+            href={`/employees/logs/${log.name}`}
+          >
+            <MoreHorizontal className="h-4 w-4" />
+          </Link>
+          <ConfirmDelete
+            confirmText={log.name}
+            deleteFn={() => handleDeleteLogs(log.name)}
+            className="rounded-md bg-slate-50 p-1 text-slate-500 hover:bg-red-100 hover:text-red-600"
+          >
+            <Trash2 className="h-4 w-4" />
+          </ConfirmDelete>
+        </div>
+        <div className="flex justify-between gap-3 text-xs text-slate-500">
+          <span>Size: {log.size}</span>
+          <div className="flex items-center gap-2">
+            <CalendarDays className="h-3 w-3 text-slate-400" />
+            <span>{dateFormater(new Date(log.createdAt))}</span>
+          </div>
         </div>
       </div>
-    </div>
-  ));
+    ))
+  ) : (
+    <i className="w-full col-span-full text-center font-medium text-lg pt-12 text-slate-400">
+      No Logs Found
+    </i>
+  );
 };
 
 export default Logs;
